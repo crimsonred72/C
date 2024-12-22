3517dddfb0c53dabbe1a7a5858c72bdd58c019bc
Abhinav <168896686+crimsonred72@users.noreply.github.com>
2024-12-22 23:14:10 +0530

    Add files via upload

---
+++ C Programs/factorials.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+ {
+     int no;
+     int factorial(int);
+     printf("Enter the Value: ");
+     scanf("%d",&no);
+     if(no>=0) 
+       {
+          printf("\n The factorial of %d is %d",no,factorial(no));
+       }
+     else 
+       {
+          printf("\n Input value must be greater than or equal to zero");
+       }
+     return 0;
+ }
+int factorial(int n)
+  {
+     if((n==0)||(n==1))
+     return 1;
+     else
+     return (n * factorial(n-1));
+  }
